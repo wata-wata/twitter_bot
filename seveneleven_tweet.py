84c79783bd91a5a4b7e00e511460cb42e5647b26
wata <60476781+wata-wata@users.noreply.github.com>
2020-11-16 11:54:23 +0900

    update files

--- seveneleven_tweet.py
+++ seveneleven_tweet.py
@@ -4,37 +4,55 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import math
 # bot
 from requests_oauthlib import OAuth1Session
 from http import HTTPStatus
 import random
 from cvs_config import * # keyを取得
 
+# ページ数を取得する
+res = requests.get('https://www.sej.co.jp/products/a/thisweek/area/kanto/1/l15/') # 1ページ目
+soup = BeautifulSoup(res.text, 'html.parser')
+# 件数(商品数)を抽出
+find1 = soup.find('div', attrs={'class': 'counter'}).text
+find2 = re.search(".*件中", find1).group()
+print(find2[:-2])
+
+base_url = 'https://www.sej.co.jp/products/a/thisweek/area/kanto/'
+base_url2 = '/l15/'
+# ページ数 = 件数 ÷ 1ページあたりの件数
+page_sum = math.ceil(int(find2[:-2]) / 15)
+print("page_sum=" + str(page_sum))
+
+# 1~page_sumの乱数をpage_numに代入する
+page_num = random.randint(1, page_sum)
+print("page_num=" + str(page_num))
+
 # スクレイピング対象の URL にリクエストを送り HTML を取得する
-res = requests.get('https://www.sej.co.jp/products/a/thisweek/area/kanto/1/l15/') # セブンイレブン新商品
+res = requests.get(base_url + str(page_num) + base_url2)
 
 # レスポンスの HTML から BeautifulSoup オブジェクトを作る
 soup = BeautifulSoup(res.text, 'html.parser')
 
 # 新商品を抽出
 find1 = soup.find_all('div', attrs={'class': 'list_inner'})
-print(find1)
 find1 = ''.join(str(find1)) # リストから文字列に変換する
 # findが使えるようにfind1を変換する
 find1 = BeautifulSoup(find1, 'html.parser')
-
-# 商品の番号
-num = 10
 
 # 商品名
 name = []
 for i in find1.find_all('div', attrs={'class': 'item_ttl'}):
     name.append(i.text) # 商品名が1つずつ入る
 
-print(name[num])
-
 N = len(name)
 print(N)
+
+# 商品の番号
+# 0~N-1の乱数をnumに代入する
+num = random.randint(0, N-1)
+
 str1 = name[num]
 
 # URL
